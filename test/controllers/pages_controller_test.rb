535a7abb80b6687839ad5d413e5a92dcb12e5c07
Prashant Vishwakarma <prashant.cllg@gmail.com>
2024-04-23 18:01:33 +0530

    - Removed webpacker. - Replaced CDNs with ruby gems. - Updated database to Postgres.

--- test/controllers/pages_controller_test.rb
+++ test/controllers/pages_controller_test.rb
@@ -5,4 +5,14 @@ class PagesControllerTest < ActionDispatch::IntegrationTest
     get pages_landing_url
     assert_response :success
   end
+
+  test "should get blog_posts" do
+    get pages_blog_posts_url
+    assert_response :success
+  end
+
+  test "should get concept_notes" do
+    get pages_concept_notes_url
+    assert_response :success
+  end
 end
